b4b24c8907289a734d1614998d6ef90467261a54
Jonathan Watson <thejmdw@gmail.com>
2022-01-19 14:05:07 -0500

    trying to sort CORS out

--- server.js
+++ server.js
@@ -5,9 +5,21 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 
+const whitelist = [
+    'http://localhost:3000',
+    'https://thejmdw.com',
+    'https://www.thejdmw.com'
+]
+
+const corsOrigin = (origin, callback) => {
+    let originISWhitelisted = whitelist.indexOf(origin) !== -1
+    callback(null, originIsWhitelisted)
+}
+
 server.use(
     cors({
-        origin: true,
+        // origin: true,
+        origin: corsOrigin(),
         credentials: true,
         preflightContinue: false,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
